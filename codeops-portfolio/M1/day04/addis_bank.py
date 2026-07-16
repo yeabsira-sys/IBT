2f3c5588e132ee335d4137aa94199d1f78fef25c
= <=>
2026-07-16 11:37:40 +0300

    day 4 project -addsi bank edited

--- codeops-portfolio/M1/day04/addis_bank.py
+++ codeops-portfolio/M1/day04/addis_bank.py
@@ -7,12 +7,16 @@ class AddisBank:
   @property
   def balance(self):
     return self.__balance
-  @balance.setter
-  def balance(self, amount):
+  # @balance.setter
+  # def balance(self, amount):
+  #   if amount <= 0:
+  #     raise ValueError("Amount cannot be negative or zero")
+  #   self.__balance += amount
+  def deposit(self, amount):
     if amount <= 0:
       raise ValueError("Amount cannot be negative or zero")
     self.__balance += amount
-  
+    return f"Deposit of {amount} successful. New balance is {self.__balance}"
   def withdraw(self, amount):
     if amount > self.__balance:
       raise ValueError("Insufficient funds")
@@ -25,9 +29,9 @@ class AddisBank:
 ac1 = AddisBank("Abebe", "123456", 1000)
 
 print(ac1.balance)
-ac1.balance = 500
+ac1.deposit(500)
 print(ac1.balance)
 ac1.withdraw(200)
-# ac1.balance = -100
+# ac1.deposit(-100)
 print(ac1.statement())
 # ac1.withdraw(2000)
